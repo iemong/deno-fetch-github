7d70bfb84c6731c7b00457691a39d78faf51bf5e
iemong <kiyoshidainagon@gmail.com>
2021-02-24 23:42:08 +0900

    スクレイピングして草のSVGを取得する

--- index.ts
+++ index.ts
@@ -1,28 +1,17 @@
-import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 
 const logger = (log: any) => {
-    console.log('data returned:', log)
-}
+  console.log("data returned:", log);
+};
 
-const query = `query GetName {
-    viewer {
-       name
-    }
-}`;
+const res = await fetch("https://github.com/iemong");
 
-const res = await fetch('https://api.github.com/graphql', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `bearer ${config().TOKEN}`,
-    Accept: 'application/json',
-  },
-  body: JSON.stringify({
-    query
-  }),
-})
+const html = await res.text();
 
-const json = await res.json()
+const document = new DOMParser().parseFromString(html, 'text/html');
 
-logger(json)
+if(!document) throw new Error('cannot get document.')
 
+const grass = document.querySelector('.js-calendar-graph-svg')
+
+logger(grass?.outerHTML);
